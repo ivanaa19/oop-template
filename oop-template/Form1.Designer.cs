b13b2ac470e5aa34a2683a1e933562cabaeb297f
ivana maksic <ivanamaksic2906@gmail.com>
2024-05-31 08:31:25 +0200

    dodata boja

--- oop-template/Form1.Designer.cs
+++ oop-template/Form1.Designer.cs
@@ -34,12 +34,13 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.button1.Location = new System.Drawing.Point(54, 28);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(173, 114);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
